b01774b8bd59e62517eea7e7992b498e35f1372d
A A Karim <adil.karim@pivigo.com>
2019-10-09 16:21:11 +0200

    Working script that takes data folder with accounts files -> CSV

---
+++ main.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"sync"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gocarina/gocsv"
+)
+
+const (
+	HTML = iota
+	XML  = iota
+)
+
+type AccountsFilingEntry struct {
+	RegistrationID string `xml:"CompaniesHouseRegisteredNumber" csv:"company_registration"`
+	Name           string `xml:"EntityNames>EntityCurrentLegalName" csv:"name"`
+	ApprovalDate   string `xml:"DateApproval" csv:"approval_date"`
+	Dormant        string `xml:"CompanyDormant" csv:"-"`
+	PeriodEnd      string `xml:"context[0]>period>endDate" csv:"-"`
+}
+
+func (c AccountsFilingEntry) String() string {
+	var result string
+	result = fmt.Sprintf("Name: %v\n", c.Name)
+	result = fmt.Sprintf("%vID: %v\n", result, c.RegistrationID)
+	result = fmt.Sprintf("%vApproval Date: %v\n", result, c.ApprovalDate)
+	result = fmt.Sprintf("%vDormant: %v\n", result, c.Dormant)
+	result = fmt.Sprintf("%vPeriod End Date: %v\n", result, c.PeriodEnd)
+	result = fmt.Sprintf("%v\n", result)
+	return result
+}
+
+func main() {
+	// ~16s without goroutines
+	// ~6s with goroutines
+	start := time.Now()
+	// load directory
+	dir, err := os.Open("./data")
+	if err != nil {
+		panic(err)
+	}
+	files, err := dir.Readdirnames(-1)
+	if err != nil {
+		panic(err)
+	}
+	// open output file before processing to see if any errors
+	output, err := os.Create("output.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer output.Close()
+	var results []AccountsFilingEntry
+	// split up IO jobs across a WaitGroup
+	var wg sync.WaitGroup
+	c := make(chan AccountsFilingEntry)
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-done:
+				fmt.Println("Closing collector goroutine")
+				return
+			case res := <-c:
+				fmt.Println(res)
+				results = append(results, res)
+			}
+		}
+	}()
+	fmt.Printf("Processing %v files\n", len(files))
+	for _, fileName := range files {
+		f, err := os.Open(path.Join("./data", fileName))
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		fileType, err := detectFileType(f)
+		if err != nil {
+			panic(err)
+		}
+		switch fileType {
+		case HTML:
+			wg.Add(1)
+			go getStuffFromHTMLInput(f, &wg, c)
+		case XML:
+			wg.Add(1)
+			go getStuffFromXMLInput(f, &wg, c)
+		}
+	}
+	wg.Wait()
+	done <- true
+	err = gocsv.MarshalFile(&results, output)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Processed %v / %v, took %v\n", len(results), len(files), time.Since(start))
+}
+
+func detectFileType(file *os.File) (int, error) {
+	ext := filepath.Ext(file.Name())
+	switch ext {
+	case ".html":
+		return HTML, nil
+	case ".xml":
+		return XML, nil
+	}
+	return 0, fmt.Errorf("could not get file ext: %v", ext)
+}
+
+func getStuffFromXMLInput(input io.Reader, wg *sync.WaitGroup, c chan AccountsFilingEntry) {
+	defer wg.Done()
+	var result AccountsFilingEntry
+	b, err := ioutil.ReadAll(input)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := xml.Unmarshal(b, &result); err != nil {
+		panic(err)
+	}
+
+	if result.RegistrationID == "" || result.Name == "" {
+		fmt.Println(result)
+		panic(string(b))
+	}
+
+	c <- result
+}
+
+func getStuffFromHTMLInput(input io.Reader, wg *sync.WaitGroup, c chan AccountsFilingEntry) {
+	defer wg.Done()
+	var result AccountsFilingEntry
+	doc, err := goquery.NewDocumentFromReader(input)
+	if err != nil {
+		panic(err)
+	}
+	// use 'contains' - * operator to search through all prefixes
+	// Assume only one result
+	doc.Find("[name*=\":UKCompaniesHouseRegisteredNumber\"]").Each(func(i int, s *goquery.Selection) {
+		result.RegistrationID = s.Text()
+	})
+
+	doc.Find("[name*=\":EntityCurrentLegalOrRegisteredName\"]").Each(func(i int, s *goquery.Selection) {
+		result.Name = s.Text()
+	})
+
+	doc.Find("[name*=\":DateAuthorisationFinancialStatementsForIssue\"]").Each(func(i int, s *goquery.Selection) {
+		result.ApprovalDate = s.Text()
+	})
+
+	doc.Find("[name*=\":EntityDormantTruefalse\"]").Each(func(i int, s *goquery.Selection) {
+		result.Dormant = s.Text()
+	})
+
+	doc.Find("[name*=\":EndDateForPeriodCoveredByReport\"]").Each(func(i int, s *goquery.Selection) {
+		result.PeriodEnd = s.Text()
+	})
+
+	if result.RegistrationID == "" || result.Name == "" {
+		html, err := doc.Html()
+		if err != nil {
+			panic(err)
+		}
+		panic(html)
+	}
+
+	c <- result
+}
