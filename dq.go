94d96965f5280fe4b7c5ec3dd61ab4771df6a2ca
A A Karim <adil.karim@pivigo.com>
2019-10-10 12:03:26 +0200

    data quality score only on exported fields

--- dq.go
+++ dq.go
@@ -7,16 +7,24 @@ func calculateDataQuality(input []AccountsFilingEntry) float32 {
 	for _, entry := range input {
 		// calcualate number of rows with non-null data
 		var currentQualScore float32
-		t := reflect.ValueOf(entry)
-		for index := 0; index < t.NumField(); index++ {
+		val := reflect.ValueOf(entry)
+		t := val.Type()
+		numFields := val.NumField()
+		for index := 0; index < numFields; index++ {
+			// ignore fields that aren't exported
+			field := val.Field(index)
+			// get struct tag from the type's field
+			if t.Field(index).Tag.Get("csv") == "-" {
+				continue
+			}
 			// Assume all fields are strings
-			str := t.Field(index).Interface()
+			str := field.Interface()
 			if str != "" {
 				currentQualScore++
 			}
 		}
 		// divide by total number of fields
-		currentQualScore = currentQualScore / float32(t.NumField())
+		currentQualScore = currentQualScore / float32(numFields)
 		totalScore += currentQualScore
 	}
 	// take average
